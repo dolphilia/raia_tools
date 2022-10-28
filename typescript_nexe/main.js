d9fbe4f50fe2f9da3f70a92d010888745291634b
dolphilia <dolphilia@outlook.com>
2022-10-28 23:10:33 +0900

    update

---
+++ typescript_nexe/main.js
@@ -0,0 +1,40 @@
+const ts = require("typescript");
+
+const source = "let x: string  = 'string';let v: string = 'hello';";
+
+let result = ts.transpileModule(source, { compilerOptions: { module: ts.ModuleKind.CommonJS }});
+
+// console.log(JSON.stringify(result));
+console.log(result.outputText);
+
+/*
+function compile(fileNames, options) {
+  let program = ts.createProgram(fileNames, options);
+  let emitResult = program.emit();
+
+  let allDiagnostics = ts
+    .getPreEmitDiagnostics(program)
+    .concat(emitResult.diagnostics);
+
+  allDiagnostics.forEach(diagnostic => {
+    if (diagnostic.file) {
+      let { line, character } = ts.getLineAndCharacterOfPosition(diagnostic.file, diagnostic.start!);
+      let message = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n");
+      console.log(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
+    } else {
+      console.log(ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n"));
+    }
+  });
+
+  let exitCode = emitResult.emitSkipped ? 1 : 0;
+  console.log(`Process exiting with code '${exitCode}'.`);
+  process.exit(exitCode);
+}
+
+compile(process.argv.slice(2), {
+  noEmitOnError: true,
+  noImplicitAny: true,
+  target: ts.ScriptTarget.ES5,
+  module: ts.ModuleKind.CommonJS
+});
+*/
